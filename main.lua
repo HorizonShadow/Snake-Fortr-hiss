dae1dac2d66a451861843bd28fe555545d7f80f4
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-21 19:25:49 -0300

    Added window title

--- main.lua
+++ main.lua
@@ -2,5 +2,6 @@ local screenWidth = 160;
 local screenHeight = 144;
 local screenScale = 3;
 function love.load()
+   love.window.setTitle("Team Fortr-hissss")
    love.window.setMode(screenWidth * screenScale, screenHeight * screenScale)
 end
