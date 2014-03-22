777717677ce49e3258eaf7bea0a9a289b00a18f0
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-21 22:51:04 -0300

    proper map updating and displaying

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- INDEXES START AT 1 FUCK
+
 local Snake = require("snake")
 local world = {
    scale = 3,
@@ -6,22 +8,28 @@ local world = {
    map = {}
 }
 local snake = nil
+local updateTimer = 0
+local scoreHeight = 14
 function love.load()
    love.window.setTitle("Team Fortr-hissss")
    love.window.setMode(world.width * world.scale, world.height * world.scale)
    love.graphics.setBackgroundColor(255,255,255)
-   for i = 1, world.width do
+   snake = Snake:new(world, 6, 6, {0, 0, 0})
+   local mapWidth = world.width / snake.width * world.scale
+   local mapHeight = ((world.height - scoreHeight) / snake.height * world.scale)
+   for i = 1, mapWidth do
       world.map[i] = {}
-      for j = 1, world.height do
+      for j = 1, mapHeight do
          world.map[i][j] = 0
       end
    end
-   snake = Snake:new(world, 6, 6, {0, 0, 0})
+   print(#world.map[1], #world.map)
+   world.map[snake.x][snake.y] = 1
 end
 
 function love.draw()
-   for i = 1, world.width do
-      for j = 1, world.height do
+   for i = 1, #world.map do
+      for j = 1, #world.map[1] do
          if world.map[i][j] > 0 then
             love.graphics.setColor(snake.color)
             love.graphics.rectangle("fill", i * snake.width, j * snake.height, snake.width, snake.height)
@@ -32,6 +40,21 @@ function love.draw()
    end
 end
 
-function love.update()
-   snake:update()
+function love.update(dt)
+   updateTimer = updateTimer + dt
+   if updateTimer > 1 then
+      world.map = snake:update()
+      print_map()
+      updateTimer = 0
+   end
+end
+
+function print_map()
+   for i = 1, #world.map do
+      for j = 1, #world.map[1] do
+         io.write(world.map[i][j])
+      end
+      print("")
+   end
+   print (" -------------------- ")
 end
