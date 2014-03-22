84b9781e81ad9830af8078574e9670a37687509a
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-22 00:04:09 -0300

    scoreboard

---
+++ scoreboard.lua
@@ -0,0 +1,24 @@
+Scoreboard = {}
+
+function Scoreboard:new(x, y, w, h, color)
+   local s = {
+      x = x,
+      y = y,
+      width = w,
+      height = h,
+      score = 0,
+      food = 0,
+      hiScore = 0
+   }
+   setmetatable(s, {__index = Scoreboard})
+   return s
+end
+
+function Scoreboard:draw()
+   love.graphics.setColor(255,255,200)
+   love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+   love.graphics.setColor(0,0,0)
+   love.graphics.print("score: "..self.score, self.x, self.y)
+   love.graphics.setColor(love.graphics.getBackgroundColor())
+end
+return Scoreboard
