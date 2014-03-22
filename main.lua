44876a159c8b5aaed8de6b88831c1a80e099852e
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-21 21:36:16 -0300

    draw the map

--- main.lua
+++ main.lua
@@ -5,13 +5,29 @@ local world = {
    height = 144,
    map = {}
 }
+local snake = nil
 function love.load()
    love.window.setTitle("Team Fortr-hissss")
-   love.window.setMode(world.width * world.scale, world.height * world.scle)
+   love.window.setMode(world.width * world.scale, world.height * world.scale)
+   love.graphics.setBackgroundColor(255,255,255)
    for i = 1, world.width do
       world.map[i] = {}
       for j = 1, world.height do
          world.map[i][j] = 0
+      end
+   end
+   snake = Snake:new(world, 1, 1, {0, 0, 0})
+end
+
+function love.draw()
+   for i = 1, world.width do
+      for j = 1, world.height do
+         if world.map[i][j] > 0 then
+            love.graphics.setColor(snake.color)
+            love.graphics.rectangle("fill", i, j, snake.width, snake.height)
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.rectangle("line", i, j, snake.width, snake.height)
+         end
       end
    end
 end
