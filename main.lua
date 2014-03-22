eeded0f8f236f3da3bbd9dda3b7cd66ad9ac9a75
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-22 02:09:45 -0300

    Rearranging some things between here and chip

--- main.lua
+++ main.lua
@@ -64,28 +64,18 @@ function draw_background_tile(x, y)
    love.graphics.rectangle("fill", ((x-1) * snake.width) + (snake.width / 4), ((y-1) * snake.height) + (snake.height / 4), snake.width / 2, snake.height / 2)
 end
 
-function draw_chip_tile(x, y)
-   love.graphics.setColor(91, 91, 91)
-   love.graphics.circle("fill", (x - 1) * snake.width + snake.width / 2, (y - 1) * snake.height + snake.height / 2, snake.width / 2, 100)
-   love.graphics.setColor(0, 0, 0)
-   love.graphics.circle("fill", (x - 1) * snake.width + snake.width / 2, (y - 1) * snake.height + snake.height / 2, snake.width / 4, 100)
-end
-
-function place_starting_chip()
-
-end
-
 function place_snake_head()
    world.map[snake.x][snake.y] = 1
 end
 
 function draw_map()
+   chip:draw()
    for i = 1, #world.map do
       for j = 1, #world.map[1] do
-         if world.map[i][j] == tile.BORDER then
+         if type(world.map[i][j]) == "table" then
+            -- do nothing
+         elseif world.map[i][j] == tile.BORDER then
             draw_background_tile(i, j)
-         elseif type(world.map[i][j]) == "table" then
-            draw_chip_tile(i, j)
          elseif world.map[i][j] > 0 then
             draw_snake_tile(i, j)
          end
@@ -116,12 +106,13 @@ end
 function init_classes()
    sboard = Scoreboard:new(0, 130 * world.scale, world.width * world.scale, 14 * world.scale)
    snake = Snake:new(6, 6, 5 * world.scale, 5 * world.scale, {0, 0, 0})
-   chip = Chip:new()
+   chip = Chip:new(0, 0, 5 * world.scale, 5 * world.scale)
 end
 
 function init_map()
-   local mapWidth = world.width / snake.width * world.scale
-   local mapHeight = ((world.height - sboard.height / world.scale) / snake.height * world.scale)
+   local mapWidth = (world.width / snake.width) * world.scale
+   local mapHeight = ((world.height * world.scale - sboard.height) / snake.width)
+   print(mapWidth, mapHeight)
    for i = 1, mapWidth do
       world.map[i] = {}
       for j = 1, mapHeight do
