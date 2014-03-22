f8981e47c718f2e240875d1081488ab4d9a69397
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-22 00:55:41 -0300

    refactoring

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 
 local Snake = require("snake")
 local Scoreboard = require("scoreboard")
-local tile = { BORDER = "BR"}
+local tile = { BORDER = "BR", CHIP = "CH"}
 local world = {
    scale = 3,
    width = 160,
@@ -12,14 +12,22 @@ local world = {
 local snake = nil
 local sboard = nil
 local updateTimer = 0
-function love.load()
+
+function init_window()
    love.window.setTitle("Team Fortr-hissss")
    love.window.setMode(world.width * world.scale, world.height * world.scale)
-   love.graphics.setBackgroundColor(255,255,255)
+end
 
+function init_graphics()
+   love.graphics.setBackgroundColor(255,255,255)
+end
+
+function init_classes()
    sboard = Scoreboard:new(0, 130 * world.scale, world.width * world.scale, 14 * world.scale)
    snake = Snake:new(world, 6, 6, {0, 0, 0})
+end
 
+function init_map()
    local mapWidth = world.width / snake.width * world.scale
    local mapHeight = ((world.height - sboard.height / world.scale) / snake.height * world.scale)
    for i = 1, mapWidth do
@@ -35,23 +43,20 @@ function love.load()
          end
       end
    end
-   print(#world.map[1], #world.map)
-   world.map[snake.x][snake.y] = 1
+end
+
+function love.load()
+   init_window()
+   init_graphics()
+   init_classes()
+   init_map()
+   place_snake_head()
+   place_starting_chip()
 end
 
 function love.draw()
    sboard:draw()
-   for i = 1, #world.map do
-      for j = 1, #world.map[1] do
-         if world.map[i][j] == tile.BORDER then
-            draw_background_tile(i, j)
-         elseif world.map[i][j] > 0 then
-            draw_snake_tile(i, j)
-         end
-         if world.map[i][j] ~= 0 then
-         end
-      end
-   end
+   draw_map()
 end
 
 function love.update(dt)
@@ -59,9 +64,7 @@ function love.update(dt)
 
    updateTimer = updateTimer + dt
    if updateTimer > 0.05 then
-      os.execute("clear")
       world.map = snake:update()
-      print_map()
       updateTimer = 0
    end
 end
@@ -78,6 +81,28 @@ function draw_background_tile(x, y)
    love.graphics.rectangle("fill", (x-1) * snake.width, (y-1) * snake.height, snake.width, snake.height)
    love.graphics.setColor(0, 0, 0)
    love.graphics.rectangle("fill", ((x-1) * snake.width) + (snake.width / 4), ((y-1) * snake.height) + (snake.height / 4), snake.width / 2, snake.height / 2)
+end
+
+function place_starting_chip()
+   --pick random x,y. Place tile.CHIP
+end
+
+function place_snake_head()
+   world.map[snake.x][snake.y] = 1
+end
+
+function draw_map()
+   for i = 1, #world.map do
+      for j = 1, #world.map[1] do
+         if world.map[i][j] == tile.BORDER then
+            draw_background_tile(i, j)
+         elseif world.map[i][j] > 0 then
+            draw_snake_tile(i, j)
+         end
+         if world.map[i][j] ~= 0 then
+         end
+      end
+   end
 end
 
 function print_map()
