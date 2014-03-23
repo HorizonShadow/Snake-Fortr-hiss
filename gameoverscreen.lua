22e0b223048e26ebcfc9410e3c408dfcea187308
Joshua LeBlanc <jleblan8@unb.ca>
2014-03-23 04:45:05 -0300

    high score posting

--- gameoverscreen.lua
+++ gameoverscreen.lua
@@ -3,13 +3,15 @@ GameOverScreen = {}
 function GameOverScreen:new(w, h)
    local g = {
       width = w,
-      height = h
+      height = h,
+      score = 0,
+      scoreText = nil
    }
    setmetatable(g, {__index = GameOverScreen})
    return g
 end
 
-function GameOverScreen:init()
+function GameOverScreen:make(s)
    local frame = loveframes.Create("frame")
       :SetName("Game Over")
       :Center()
@@ -30,9 +32,45 @@ function GameOverScreen:init()
    local mainMenuButton = loveframes.Create("button", frame)
       :SetText("Main Menu")
       :SetPos(200, 350)
+
+   local nameInput = loveframes.Create("textinput", frame)
+      :SetPos(100, 100)
+      :SetWidth(200)
+   local scoreText = loveframes.Create("text", frame)
+      :SetPos(100, 150)
+      :SetText(s)
+   local submitHighscore = loveframes.Create("button", frame)
+      :SetPos(100, 175)
+   submitHighscore.OnClick =
+      function(object, x, y)
+         submit_highscore(s, nameInput:GetText())
+      end
    playButton.OnClick = on_play_button_click
    storeButton.OnClick = on_store_button_click
    mainMenuButton.OnClick = on_main_menu_button_click
+end
+
+function submit_highscore(score, name)
+   print("working")
+   local http = require("socket.http")
+   local ltn12 = require("ltn12")
+   print(http)
+   local reqbody = "name="..name.."&score="..score
+   local respbody = {} -- for the response body
+
+   local result, respcode, respheaders, respstatus = http.request {
+      method = "POST",
+      url = "http://obscure-spire-3612.herokuapp.com/submit",
+      source = ltn12.source.string(reqbody),
+      headers = {
+         ["content-type"] = "application/x-www-form-urlencoded",
+         ["content-length"] = tostring(#reqbody)
+      },
+      sink = ltn12.sink.table(respbody)
+   }
+   -- get body as string by concatenating table filled by sink
+   respbody = table.concat(respbody)
+   print("respbody", respcode)
 end
 
 function on_play_button_click()
